358ade1b8918840671ed291d957ff95e09db497e
prageer <prageer@cheerful.com>
2018-02-21 06:20:22 -0800

    created RegisterTemplate

--- src/components/templates/RegisterTemplate/RegisterTemplate.js
+++ src/components/templates/RegisterTemplate/RegisterTemplate.js
@@ -1,9 +1,30 @@
+// @flow
 import React, { Component } from 'react';
+import BackgroundColor from 'components/common/BackgroundColor';
+import Responsive from 'components/common/Responsive';
 
-class RegisterTemplate extends Component {
+import './RegisterTemplate.scss';
+
+type Props = {};
+
+class RegisterTemplate extends Component<Props> {
   render() {
     return (
-      <div>
+      <div className="register-template">
+        <BackgroundColor color="#495057" />
+        <Responsive className="mock-header">
+          <div className="brand">
+            Prageer
+          </div>
+          <div className="light">
+            <span>/</span>Member Login
+          </div>
+        </Responsive>
+        <section className="rest">
+          <div className="register-card">
+            hello
+          </div>
+        </section>
       </div>
     );
   }
